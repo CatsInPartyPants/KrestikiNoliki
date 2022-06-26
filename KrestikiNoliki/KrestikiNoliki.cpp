ee72c6d9cc0e0db1812b550af21198589ba65198
Artem Nilov <nilov1235@mail.ru>
2022-06-26 18:40:11 +0300

    krestiki noliki

--- KrestikiNoliki/KrestikiNoliki.cpp
+++ KrestikiNoliki/KrestikiNoliki.cpp
@@ -1,28 +1,73 @@
 ﻿#include <iostream>
 #include "main_menu.h"
 
+char board[3][3];
 // отображение поля
-void show(int a, int b, const char c) {
+void show() {
 	cout << "  1 2 3 \n";
 	for (int i = 0; i < 3; i++)
 	{
 		cout << i + 1;
 		for (int j = 0; j < 3; j++)
 		{
-			if(i == a-1 && j == b-1)
-				cout << "|" << c;
-			else 
-				cout << "|" << "_";
+				cout << "|" << board[i][j];
 		}
 		cout << "|\n";
 	}
+}
+//условия победы (доделать)
+bool win()
+{
+	if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] !='_') return false;
+	if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != '_') return false;
+	if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != '_') return false;
+
+	if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != '_') return false;
+	if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != '_') return false;
+	if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != '_') return false;
+
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '_') return false;
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '_') return false;
+	return true;
+}
+
+void stepPlayer(int i)
+{
+	int c1, c2;
+	do {
+		cout << "Выберете значение по горизонтали: ";
+		cin >> c1;
+		cout << "Выберете значение по вертикали: ";
+		cin >> c2;
+		if (board[c1-1][c2-1] == 'X' || board[c1-1][c2-1] == 'O')
+		{
+			cout << "Клетка занята. Выберете другую клетку\n";
+		}
+	} while (board[c1-1][c2-1] != '_');
+	i ? board[--c1][--c2] = X : board[--c1][--c2] = O;
+	show();
 }
 
 
 int main()
 {
 	setlocale(LC_ALL, "");
-	//menu();
-	show(1, 2, X);
+	// заполняем массив значениями
+	for(int i = 0; i< 3; i++)
+	{
+		for(int j = 0; j< 3; j++)
+		{
+			board[i][j] = '_';
+		}
+	}
+	menu();
+	show();
+	while(win())
+	{
+		cout << "Ход игрока " << firstPlayer << endl;
+		stepPlayer(nF);
+		if (!win()) break;
+		cout << "Ход игрока " << secondPlayer << endl;
+		stepPlayer(nS);
+	}
 }
-
--- KrestikiNoliki/main_menu.h
+++ KrestikiNoliki/main_menu.h
@@ -2,30 +2,43 @@
 using namespace std;
 #define X 'X'
 #define O 'O'
-
+int nS;
+int nF = nS = 0;
 char firstPlayer[50];
 char secondPlayer[50];
 
 void EnterName()
 {
-	cout << "Введите имя первого игрока: ";
-	cin >> firstPlayer;
-	cout << "Введите имя второго игрока: ";
-	cin >> secondPlayer;
+    if (firstPlayer) {
+        cout << "Введите имя первого игрока: ";
+        cin >> firstPlayer;
+    }
+    if (secondPlayer) {
+        cout << "Введите имя второго игрока: ";
+        cin >> secondPlayer;
+    }
 }
 
 void ChoiceChar()
 {
-    cout << "За какой символ играет игрок " << firstPlayer << "? (X или O) \n";
+    char c;
+    if (firstPlayer != NULL || secondPlayer!= NULL) {
+        cout << "За какой символ играет игрок " << firstPlayer << "? (X или O) \n";
+        cin >> c;
+        (c == 'X') ? nF = 1 : nS = 1;
+    }
+    else
+        cout << "Имена игроков не введены.\n";
 }
 
 void menu()
 {   
     char c;
+    bool flag = true;
     do
     {
     cout << "Игра \"Крестики - нолики\"\n\n";
-    cout << "1. Ввести имя игрока\n";
+    cout << "1. Ввести имя игрока" << firstPlayer << " " << secondPlayer << "\n";
     cout << "2. Выбор символа(крестик или нолик)\n";
     cout << "3. Выход из игры\n";
     
@@ -38,6 +51,7 @@ void menu()
             break;
         case '2':
             ChoiceChar();
+            flag = false;
             break;
         case '3':
             exit(0);
@@ -45,5 +59,5 @@ void menu()
         default:
             break;
         }
-    } while (c != '3');
+    } while (c != '3' && flag);
 }
